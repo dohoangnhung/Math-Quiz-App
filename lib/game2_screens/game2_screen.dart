1e00b03ec3a03984f59c7c8cbc63266e746a6961
Đỗ Hoàng Nhung <74888302+dohoangnhung@users.noreply.github.com>
2023-03-15 15:19:41 +0700

    update game2 screen

--- lib/game2_screens/game2_screen.dart
+++ lib/game2_screens/game2_screen.dart
@@ -1,1 +1,115 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:quickalert/quickalert.dart';
 
+import 'package:math_quiz/controllers/game2_controller.dart';
+import '../navigator.dart';
+import 'components/g2_body.dart';
+
+class Game2Screen extends StatelessWidget {
+  const Game2Screen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Game2Controller _controller = Get.put(Game2Controller());
+    return WillPopScope(
+      onWillPop: () async {
+        QuickAlert.show(
+          context: context,
+          type: QuickAlertType.warning,
+          title: 'Cảnh báo',
+          text: 'Bạn có muốn thoát khỏi trò chơi?',
+          textColor: const Color.fromARGB(255, 60, 60, 60),
+          confirmBtnText: 'Có',
+          confirmBtnColor: const Color.fromARGB(255, 4, 114, 117),
+          onConfirmBtnTap: () {
+            Get.offAll(() => const NavigatorPage());
+          },
+          confirmBtnTextStyle: const TextStyle(
+            color: Colors.white,
+            fontSize: 18,
+            fontWeight: FontWeight.normal,
+          ),
+          showCancelBtn: true,
+          cancelBtnText: 'Không',
+          onCancelBtnTap: () {
+            Get.back();
+          },
+          cancelBtnTextStyle: const TextStyle(
+            color: Colors.grey,
+            fontSize: 18,
+            fontWeight: FontWeight.normal,
+          ),
+        );
+        return true;
+      },
+      child: Scaffold(
+        extendBodyBehindAppBar: true,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          leading: TextButton(
+            onPressed: () {
+              QuickAlert.show(
+                context: context,
+                type: QuickAlertType.warning,
+                title: 'Cảnh báo',
+                text: 'Bạn có muốn thoát khỏi trò chơi?',
+                textColor: const Color.fromARGB(255, 60, 60, 60),
+                confirmBtnText: 'Có',
+                confirmBtnColor: const Color.fromARGB(255, 4, 114, 117),
+                onConfirmBtnTap: () {
+                  Get.offAll(() => const NavigatorPage());
+                },
+                confirmBtnTextStyle: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  fontWeight: FontWeight.normal,
+                ),
+                showCancelBtn: true,
+                cancelBtnText: 'Không',
+                onCancelBtnTap: () {
+                  Get.back();
+                },
+                cancelBtnTextStyle: const TextStyle(
+                  color: Colors.grey,
+                  fontSize: 18,
+                  fontWeight: FontWeight.normal,
+                ),
+              );
+            },
+            child: Image.asset(
+              'assets/go-back.png',
+              scale: 2.5,
+            ),
+          ),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 10),
+              child: TextButton(
+                onPressed: _controller.nextQuestion,
+                child: const Text(
+                  "Bỏ qua",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            )
+          ],
+        ),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 58, 65, 127),
+                Color.fromARGB(255, 21, 23, 81),
+              ],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
+          ),
+          child: const Body(),
+        ),
+      ),
+    );
+  }
+}
