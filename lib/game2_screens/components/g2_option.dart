541f4d8d46c841b3f023f2b0b86e17c3763067c2
Đỗ Hoàng Nhung <74888302+dohoangnhung@users.noreply.github.com>
2023-03-16 00:55:21 +0700

    update game2 option

--- lib/game2_screens/components/g2_option.dart
+++ lib/game2_screens/components/g2_option.dart
@@ -1,1 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:get/get_state_manager/get_state_manager.dart';
 
+import '../../controllers/game2_controller.dart';
+
+class Option extends StatelessWidget {
+  final String text;
+  final int index;
+  final VoidCallback press;
+
+  const Option({
+    super.key,
+    required this.text,
+    required this.index,
+    required this.press,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GetBuilder<Game2Controller>(
+        init: Game2Controller(),
+        builder: (controller) {
+          Color getTheRightColor() {
+            if (controller.isAnswered) {
+              if (controller.correctAns.contains(index)) {
+                return Colors.green;
+              } else if (controller.selectedAns.contains(index) &&
+                  !controller.equal(
+                      controller.selectedAns, controller.correctAns)) {
+                return Colors.red;
+              }
+            }
+            return const Color.fromRGBO(193, 193, 193, 1);
+          }
+
+          IconData getTheRightIcon() {
+            return getTheRightColor() == Colors.red ? Icons.close : Icons.done;
+          }
+
+          Color fillOptionColor() {
+            if (controller.isAnswered) {
+              if (controller.correctAns.contains(index)) {
+                return const Color.fromARGB(255, 233, 241, 231);
+              } else if (controller.selectedAns.contains(index) &&
+                  !controller.equal(
+                      controller.selectedAns, controller.correctAns)) {
+                return const Color.fromARGB(255, 245, 227, 227);
+              }
+            }
+            return Colors.white;
+          }
+
+          return InkWell(
+            onTap: press,
+            child: Container(
+              margin: const EdgeInsets.only(top: 20),
+              padding: const EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                color: fillOptionColor(),
+                border: Border.all(color: getTheRightColor()),
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    text,
+                    style: TextStyle(
+                      color: getTheRightColor(),
+                      fontSize: 16,
+                    ),
+                  ),
+                  Container(
+                    height: 26,
+                    width: 26,
+                    decoration: BoxDecoration(
+                      color: getTheRightColor() ==
+                              const Color.fromRGBO(193, 193, 193, 1)
+                          ? Colors.transparent
+                          : getTheRightColor(),
+                      border: Border.all(
+                        color: getTheRightColor(),
+                      ),
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                    child: Icon(
+                      getTheRightIcon(),
+                      size: 16,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        });
+  }
+}
