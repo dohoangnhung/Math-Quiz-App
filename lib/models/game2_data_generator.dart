f4548c91ae44b223708f301bf2e0da16750a4b7d
Đỗ Hoàng Nhung <74888302+dohoangnhung@users.noreply.github.com>
2023-03-15 23:09:45 +0700

    create game2 data generator

--- lib/models/game2_data_generator.dart
+++ lib/models/game2_data_generator.dart
@@ -1,229 +1,39 @@
 import 'dart:math';
 
 class Game2DataGenerator {
-  final List<String> listOfOps = ['+', '-', 'x', ':'];
+  List<int> generateOptions(int sum, int numOfOptions) {
+    // to avoid duplicated element
+    Set<int> optionsSet = {};
 
-  Pair oneNumExp() {
-    int num = Random().nextInt(100) + 1;
-    return Pair(num.toString(), num);
-  }
+    // generate the seed (1 of 2 correct answers)
+    int num = Random().nextInt(sum - 1) + 1;
 
-  Pair twoNumExp() {
-    var op = listOfOps[Random().nextInt(listOfOps.length)];
+    optionsSet.add(num);
+    do {
+      int element = Random().nextInt(sum - 1) + 1;
+      optionsSet.add(element);
 
-    int num1 = 1;
-    int num2 = 1;
-    late int result;
-
-    if (op == '+') {
-      num1 = Random().nextInt(21) + 10;
-      num2 = Random().nextInt(9) + 1;
-      result = num1 + num2;
-    }
-
-    if (op == '-') {
-      num1 = Random().nextInt(21) + 10;
-      num2 = Random().nextInt(9) + 1;
-      result = num1 - num2;
-    }
-
-    if (op == 'x') {
-      num1 = Random().nextInt(9) + 1;
-      num2 = Random().nextInt(8) + 2;
-      result = num1 * num2;
-    }
-
-    if (op == ':') {
-      // check if the dividend is an prime number
-      do {
-        num1 = Random().nextInt(96) + 4;
-        num2 = Random().nextInt(8) + 2;
-      } while (isPrime(num1));
-
-      // to ensure that dividend % divisor == 0 and dividend != divisor
-      if (num1 % num2 != 0 || num1 == num2) {
-        for (int i = num2 - 1; i > 1; i--) {
-          if (num1 % i == 0) {
-            num2 = i;
-            break;
-          }
-        }
+      // to ensure that the question only has 1 correct answer
+      if (!onlyOneAns(optionsSet.toList(), sum)) {
+        optionsSet.remove(element);
       }
-      result = num1 ~/ num2;
-    }
-    return Pair('$num1 $op $num2', result);
+    } while (optionsSet.length != numOfOptions - 1);
+
+    // for 5 + 5 situation
+    List<int> optionsList = optionsSet.toList();
+    optionsList.add(sum - num);
+
+    return optionsList;
   }
 
-  Pair get generateTwoNumExp => twoNumExp();
-
-  Pair threeNumExp() {
-    var op = listOfOps[Random().nextInt(listOfOps.length)];
-
-    int num = 1;
-    late int result;
-
-    Pair genPrvTwoNumExp = Pair('', 0);
-    String twoNumExp;
-    int prvResult;
-
-    if (op == '+') {
-      genPrvTwoNumExp = generateTwoNumExp;
-      prvResult = genPrvTwoNumExp.value;
-      num = Random().nextInt(9) + 1;
-      result = prvResult + num;
-    }
-
-    if (op == '-') {
-      do {
-        genPrvTwoNumExp = generateTwoNumExp;
-        prvResult = genPrvTwoNumExp.value;
-        num = Random().nextInt(9) + 1;
-      } while (prvResult < num);
-      result = prvResult - num;
-    }
-
-    if (op == 'x') {
-      do {
-        genPrvTwoNumExp = generateTwoNumExp;
-        prvResult = genPrvTwoNumExp.value;
-        num = Random().nextInt(8) + 2;
-      } while (prvResult * num >= 100);
-      result = prvResult * num;
-    }
-
-    if (op == ':') {
-      // check if the dividend is an prime number
-      do {
-        genPrvTwoNumExp = generateTwoNumExp;
-        prvResult = genPrvTwoNumExp.value;
-        num = Random().nextInt(8) + 2;
-      } while (isPrime(prvResult));
-
-      // to ensure that dividend % divisor == 0
-      if (prvResult % num != 0 || prvResult == num) {
-        for (int i = num - 1; i > 1; i--) {
-          if (prvResult % i == 0) {
-            num = i;
-            break;
-          }
+  bool onlyOneAns(List<int> optionsList, int sum) {
+    for (int i = 0; i < optionsList.length - 1; i++) {
+      for (int j = i + 1; j < optionsList.length; j++) {
+        if (optionsList[i] + optionsList[j] == sum) {
+          return false;
         }
-      }
-      result = prvResult ~/ num;
-    }
-    twoNumExp = genPrvTwoNumExp.exp;
-
-    return Pair('($twoNumExp) $op $num', result);
-  }
-
-  Pair get generateThreeNumExp => threeNumExp();
-
-  // TODO: write code =))
-  Pair fourNumExp() {
-    return Pair('', 0);
-  }
-
-  List<Pair> questionLv1() {
-    Pair exp1, exp2;
-    do {
-      exp1 = oneNumExp();
-      exp2 = oneNumExp();
-    } while (exp1.value == exp2.value);
-
-    return [exp1, exp2];
-  }
-
-  List<Pair> get genQuestionLv1 => questionLv1();
-
-  List<Pair> questionLv2() {
-    Pair exp1, exp2;
-    do {
-      exp1 = twoNumExp();
-      exp2 = oneNumExp();
-    } while (exp1.value == exp2.value);
-
-    return [exp1, exp2];
-  }
-
-  List<Pair> get genQuestionLv2 => questionLv2();
-
-  List<Pair> questionLv3() {
-    Pair exp1, exp2;
-    do {
-      exp1 = twoNumExp();
-      exp2 = twoNumExp();
-    } while (exp1.value == exp2.value);
-
-    return [exp1, exp2];
-  }
-
-  List<Pair> get genQuestionLv3 => questionLv3();
-
-  List<Pair> questionLv4() {
-    Pair exp1, exp2;
-    do {
-      exp1 = threeNumExp();
-      exp2 = twoNumExp();
-    } while (exp1.value == exp2.value);
-
-    return [exp1, exp2];
-  }
-
-  List<Pair> get genQuestionLv4 => questionLv4();
-
-  List<Pair> questionLv5() {
-    Pair exp1, exp2;
-    do {
-      exp1 = threeNumExp();
-      exp2 = threeNumExp();
-    } while (exp1.value == exp2.value);
-
-    return [exp1, exp2];
-  }
-
-  List<Pair> get genQuestionLv5 => questionLv5();
-
-  List<Pair> questionLv6() {
-    Pair exp1, exp2, exp3;
-    do {
-      exp1 = fourNumExp();
-      exp2 = threeNumExp();
-      exp3 = twoNumExp();
-    } while (exp1.value == exp2.value && exp2.value == exp3.value);
-
-    return [exp1, exp2, exp3];
-  }
-
-  List<Pair> get genQuestionLv6 => questionLv6();
-
-  List<Pair> questionLv7() {
-    Pair exp1, exp2, exp3;
-    do {
-      exp1 = fourNumExp();
-      exp2 = fourNumExp();
-      exp3 = twoNumExp();
-    } while (exp1.value == exp2.value && exp2.value == exp3.value);
-
-    return [exp1, exp2, exp3];
-  }
-
-  List<Pair> get genQuestionLv7 => questionLv7();
-
-  bool isPrime(int num) {
-    for (var i = 2; i < num; i++) {
-      if (num % i == 0) {
-        return false;
       }
     }
     return true;
   }
-}
-
-class Pair {
-  final String exp;
-  final int value;
-
-  Pair(this.exp, this.value);
-
-  @override
-  String toString() => 'Pair($exp, $value)';
 }
