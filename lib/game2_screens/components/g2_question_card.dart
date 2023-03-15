0d54a69962148bb45e2d07ac79ae9c80909c3b7a
Đỗ Hoàng Nhung <74888302+dohoangnhung@users.noreply.github.com>
2023-03-16 00:51:15 +0700

    create game2 question card

--- lib/game2_screens/components/g2_question_card.dart
+++ lib/game2_screens/components/g2_question_card.dart
@@ -1,1 +1,55 @@
+import "package:flutter/material.dart";
+import "package:get/get.dart";
+import "package:math_quiz/controllers/game2_controller.dart";
 
+import 'g2_option.dart';
+
+class QuestionCard extends StatelessWidget {
+  final List<int> question;
+
+  const QuestionCard({
+    Key? key,
+    required this.question,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Game2Controller _controller = Get.put(Game2Controller());
+    return Container(
+      margin: const EdgeInsets.fromLTRB(20, 20, 20, 30),
+      padding: const EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(25),
+      ),
+      child: Column(
+        children: [
+          Text(
+            // TODO: chia level
+            'Hai số nào dưới đây có tổng bằng 10?',
+            style: Theme.of(context)
+                .textTheme
+                .headlineSmall!
+                .copyWith(color: const Color.fromRGBO(16, 16, 16, 1)),
+          ),
+          const SizedBox(height: 10),
+          ...List.generate(
+            question.length,
+            (index) => Option(
+              index: index,
+              text: question[index].toString(),
+              press: () => {
+                // choose two options at a time
+                _controller.optionsChosen.add(index),
+                if (_controller.optionsChosen.length == 2)
+                  {
+                    _controller.checkAns(question, _controller.optionsChosen),
+                  }
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
