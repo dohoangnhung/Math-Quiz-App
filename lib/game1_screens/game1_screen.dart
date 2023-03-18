e2cde589eba2d0f902ae4a3594c233b3c89c3987
Đỗ Hoàng Nhung <74888302+dohoangnhung@users.noreply.github.com>
2023-03-18 10:17:16 +0700

    write function for quickAlert dialog

--- lib/game1_screens/game1_screen.dart
+++ lib/game1_screens/game1_screen.dart
@@ -14,33 +14,7 @@ class Game1Screen extends StatelessWidget {
     Game1Controller _controller = Get.put(Game1Controller());
     return WillPopScope(
       onWillPop: () async {
-        QuickAlert.show(
-          context: context,
-          type: QuickAlertType.warning,
-          title: 'Cảnh báo',
-          text: 'Bạn có muốn thoát khỏi trò chơi?',
-          textColor: const Color.fromARGB(255, 60, 60, 60),
-          confirmBtnText: 'Có',
-          confirmBtnColor: const Color.fromARGB(255, 4, 114, 117),
-          onConfirmBtnTap: () {
-            Get.offAll(() => const NavigatorPage());
-          },
-          confirmBtnTextStyle: const TextStyle(
-            color: Colors.white,
-            fontSize: 18,
-            fontWeight: FontWeight.normal,
-          ),
-          showCancelBtn: true,
-          cancelBtnText: 'Không',
-          onCancelBtnTap: () {
-            Get.back();
-          },
-          cancelBtnTextStyle: const TextStyle(
-            color: Colors.grey,
-            fontSize: 18,
-            fontWeight: FontWeight.normal,
-          ),
-        );
+        showExitDialog(context);
         return true;
       },
       child: Scaffold(
@@ -50,33 +24,7 @@ class Game1Screen extends StatelessWidget {
           elevation: 0,
           leading: TextButton(
             onPressed: () {
-              QuickAlert.show(
-                context: context,
-                type: QuickAlertType.warning,
-                title: 'Cảnh báo',
-                text: 'Bạn có muốn thoát khỏi trò chơi?',
-                textColor: const Color.fromARGB(255, 60, 60, 60),
-                confirmBtnText: 'Có',
-                confirmBtnColor: const Color.fromARGB(255, 4, 114, 117),
-                onConfirmBtnTap: () {
-                  Get.offAll(() => const NavigatorPage());
-                },
-                confirmBtnTextStyle: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 18,
-                  fontWeight: FontWeight.normal,
-                ),
-                showCancelBtn: true,
-                cancelBtnText: 'Không',
-                onCancelBtnTap: () {
-                  Get.back();
-                },
-                cancelBtnTextStyle: const TextStyle(
-                  color: Colors.grey,
-                  fontSize: 18,
-                  fontWeight: FontWeight.normal,
-                ),
-              );
+              showExitDialog(context);
             },
             child: Image.asset(
               'assets/go-back.png',
@@ -112,4 +60,34 @@ class Game1Screen extends StatelessWidget {
       ),
     );
   }
+}
+
+void showExitDialog(BuildContext context) {
+  QuickAlert.show(
+    context: context,
+    type: QuickAlertType.warning,
+    title: 'Cảnh báo',
+    text: 'Bạn có muốn thoát khỏi trò chơi?',
+    textColor: const Color.fromARGB(255, 60, 60, 60),
+    confirmBtnText: 'Có',
+    confirmBtnColor: const Color.fromARGB(255, 4, 114, 117),
+    onConfirmBtnTap: () {
+      Get.offAll(() => const NavigatorPage());
+    },
+    confirmBtnTextStyle: const TextStyle(
+      color: Colors.white,
+      fontSize: 18,
+      fontWeight: FontWeight.normal,
+    ),
+    showCancelBtn: true,
+    cancelBtnText: 'Không',
+    onCancelBtnTap: () {
+      Get.back();
+    },
+    cancelBtnTextStyle: const TextStyle(
+      color: Colors.grey,
+      fontSize: 18,
+      fontWeight: FontWeight.normal,
+    ),
+  );
 }
