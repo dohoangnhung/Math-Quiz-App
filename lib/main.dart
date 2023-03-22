bdb0fabc2355a2db95119efdbee4601ae96ac90a
Đỗ Hoàng Nhung <74888302+dohoangnhung@users.noreply.github.com>
2023-03-22 11:46:57 +0700

    add notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,42 @@
+import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/get_navigation.dart';
 import 'package:math_quiz/navigator.dart';
+import 'package:math_quiz/notification/reminder_screen.dart';
 
 void main() {
+  AwesomeNotifications().initialize(
+    null,
+    [
+      NotificationChannel(
+        channelKey: 'reminder',
+        channelName: 'Daily Reminder',
+        channelDescription:
+            'This daily reminder is designed to remind people to play the game every day.',
+      ),
+    ],
+    debug: true,
+  );
   runApp(const App());
 }
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  void initState() {
+    AwesomeNotifications().isNotificationAllowed().then((isAllowed) {
+      if (!isAllowed) {
+        AwesomeNotifications().requestPermissionToSendNotifications();
+      }
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +44,7 @@ class App extends StatelessWidget {
       title: 'Quiz App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(fontFamily: 'RobotoSlab'),
-      home: const NavigatorPage(),
+      home: const ReminderAllowScr(),
     );
   }
 }
